35ab3aa9aba33e8defaee13e4d62b4e532b7b49f
Wilmer <wilmer_will@outlook.com>
2022-02-28 22:40:15 -0500

    Wordiness Comepleted

--- Grammark2.0/Grammark/src/app/home/fixes/wordiness-fix/wordiness-fix.component.ts
+++ Grammark2.0/Grammark/src/app/home/fixes/wordiness-fix/wordiness-fix.component.ts
@@ -105,6 +105,11 @@ export class WordinessFixComponent implements OnInit {
     this.data.changeMessage('');
   }
 
+         //returns the element that is displayed in the html
+         getContent() {
+          return document.getElementById('userinput').innerHTML;
+        }
+
   reHighlight(): void {
     // Reset every time you hit re-highlight
     this.data.changeTotalSentences(0);
@@ -124,13 +129,13 @@ export class WordinessFixComponent implements OnInit {
     this.transitionsUserTable = { find: [], suggestion: [] };
     this.grammarUserTable = { find: [], suggestion: [] };
     this.eggcornsUserTable = { find: [], suggestion: [] };
-    this.academicStyleUserTable = { find: [], suggestion: [] };
+    this.academicStyleUserTable = [];
     this.nominalizationsUserTable = { find: [], suggestion: [] };
     this.sentencesUserTable = { find: [], suggestion: [] };
 
     // variables
-    // tslint:disable-next-line: prefer-const
-    let userText = (document.getElementById('userinput') as HTMLTextAreaElement).value;
+    // user text = paragraph from the html file
+    let userText = this.getContent();
     let aLetter = false;
 
     // This function checks if there is at least one letter inputed
@@ -138,24 +143,25 @@ export class WordinessFixComponent implements OnInit {
       if (/[a-zA-Z]/.test(userText)) {
         aLetter = true;
       }
-    }
-
+    };
     // calling function - checker
     validateChar();
 
     // alters! or proceed to overview
     if (userText === '') {
       alert('Please fill out the text area');
-    }
-    else if (aLetter === false) {
+    } else if (aLetter === false) {
       alert('Please enter at least one letter');
-    }
-    else {
+    } else {
       this.data.changeMessage(userText);
 
       // Find total sentences in text
       for (let i = 0; i < userText.length; i++) {
-        if (userText.charAt(i) === "." || userText.charAt(i) === "!" || userText.charAt(i) === "?") {
+        if (
+          userText.charAt(i) === '.' ||
+          userText.charAt(i) === '!' ||
+          userText.charAt(i) === '?'
+        ) {
           this.data.changeTotalSentences(this.totalSentences + 1);
         }
       }
@@ -243,7 +249,7 @@ export class WordinessFixComponent implements OnInit {
     for (const fix in this.academicStyleTable) {
       if (userText.includes(fix)) {
         this.academic.changeTotalNonAcademic(this.totalNonAcademic + 1);
-        this.academicStyleUserTable.find.push("• " + fix + " ⟶ " + this.academicStyleTable[fix]);
+        //this.academicStyleUserTable.find.push("• " + fix + " ⟶ " + this.academicStyleTable[fix]);
         this.academic.changeAcademicStyleUserTable(this.academicStyleUserTable);
         // this.academicStyleUserTable.suggestion.push("→ " + this.academicStyleTable[fix]);
       }
@@ -536,6 +542,7 @@ export class WordinessFixComponent implements OnInit {
         this.wordinessUserTable.find.push("• " + fix + " ⟶ " + this.wordinessTable[fix]);
         // this.wordinessUserTable.suggestion.push("→ " + this.wordinessTable[fix]);
         this.wordiness.changeWordinessUserTable(this.wordinessUserTable);
+        this.highlight(fix)
       }
     }
     this.wordinessScore = (this.wordinessNumber / this.totalSentences) * 100;
@@ -746,5 +753,21 @@ export class WordinessFixComponent implements OnInit {
     // Transitions score
     this.transitions.currentTransitionsScore.subscribe(transitionsScore => this.transitionsScore = transitionsScore);
 
+  }
+
+  highlight(text) {
+    //hold the message from the html textbox with id= userinput
+    var paragraph = document.getElementById('userinput');
+    
+    //dynamic/custom regex expression -> only way to use variable inside regex
+    let re = new RegExp(`\\b${text}\\b`, 'gi');
+
+    //replace with -> span and highlight
+    paragraph.innerHTML = paragraph.innerHTML.replace(
+      re,
+      '<span style="background-color: #FF6363; font-family: Georgia;" >' +
+        text +
+        ' </span>'
+    );
   }
 }
